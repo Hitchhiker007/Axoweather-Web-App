d61d277ee419a04de93236dff322f2dd0c81cbb9
Hitchhiker007 <80670903+Hitchhiker007@users.noreply.github.com>
2025-03-28 00:51:58 +1100

    started on fixing curr location seciton

--- front-end/weather-app/app/page.tsx
+++ front-end/weather-app/app/page.tsx
@@ -11,15 +11,11 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>What's The Weather?</h1>
-        <Image
-          className="dark:invert"
-          src="/title.png"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <div className="container flex items-center gap-4">
+        <div className="text flex flex-col">
         <TimeDisplay />
+          </div>
+        </div>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by selecting your location.
