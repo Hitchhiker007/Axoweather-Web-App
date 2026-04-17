9c91eb0ac24de80eaa07aeec171fea40588c5434
Hitchhiker007 <80670903+Hitchhiker007@users.noreply.github.com>
2026-04-17 18:07:37 +1000

    lets go test!

--- weather-api/front-end/app/page.tsx
+++ weather-api/front-end/app/page.tsx
@@ -27,6 +27,7 @@ export default function Home() {
           
 
         {/* tirgger a workflow test */}
+        {/* TEST 2! */}
         <FetchWeather />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
